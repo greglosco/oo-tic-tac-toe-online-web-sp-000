648956177b0e8f32315a58a397a6fb51403cee14
Gregory Losco <gtlosco@gmail.com>
2019-12-15 16:25:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,7 +35,7 @@ WIN_COMBINATIONS = [
     !(@board[index].nil? || @board[index] == " ")
   end
   
-  def valid_move?(@board, index)
+  def valid_move?(board, index)
     if index.between?(0,8) && !position_taken?(@board,index)
      true
     else
